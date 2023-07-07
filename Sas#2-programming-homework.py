ef151b8b06c0043fffcc2487b8f3496e78bbbf4c
John Paul C, Nieto <114987652+johnpaulll@users.noreply.github.com>
2023-07-07 15:32:18 +0800

    Add files via upload

---
+++ Sas#2-programming-homework.py
@@ -0,0 +1,6 @@
+print("====================================================")
+length = float(input("ENTER THE LENGTH OF TRIANGLE\n: "))
+width = float(input("ENTER THE WIDTH OF TRIANGLE\n:"))
+area = length * width
+print("THE AREA OF RECTANGLE IS\n:", area)
+print("====================================================")
