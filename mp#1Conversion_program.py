5d1c8f54d42ee5e05085599bd5ab4cf1e9668825
John Paul C, Nieto <114987652+johnpaulll@users.noreply.github.com>
2023-07-20 14:34:00 +0800

    Add files via upload

---
+++ mp#1Conversion_program.py
@@ -0,0 +1,18 @@
+gram = float(input("ENTER A GRAM:\n"))
+
+kilogram = gram / 1000
+milligram = gram * 1000
+microgram = gram * 1000000
+pound = gram * 453.6
+ounce = gram / 28.35
+liter = gram / 1000
+
+print("=========================================")
+print("10 GRAM TO TO KILOGRAM:", kilogram)
+print("10 GRAM TO TO KILOGRAM:", milligram)
+print("10 GRAM TO TO KILOGRAM:", microgram)
+print("10 GRAM TO TO KILOGRAM:", pound)
+print("10 GRAM TO TO KILOGRAM:", ounce)
+print("10 GRAM TO TO KILOGRAM:", liter)
+print("=========================================")
+print("==============@NIETO=====================")
